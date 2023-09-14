94a80a020402c408ae74a8989c5b851a688b888a
legop3 <minecratfd8@gmail.com>
2023-09-13 20:02:00 -0400

    first

---
+++ gpio-led.py
@@ -0,0 +1,18 @@
+from threading import Event
+import _thread
+import logging
+import time
+
+import pwnagotchi.plugins as plugins
+
+class GpioLed(plugins.Plugin):
+    __author__='legop3'
+    __version__='1.0.0'
+    __license__='GPL3'
+    __description__='allows you to set a GPIO pin to blink for certain events'
+
+    def __init__(self):
+        self.running = False
+
+    def on_loaded(self):
+        logging.info("gpio-led plugin loaded for %s" % self.options['device'])
